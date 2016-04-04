f511423ece0ed21f36c612b8c7afa30d182f3c88
Min Kwon <s4286260@gmail.com>
2016-04-04 21:49:34 +0800

    implemneted basic function that draws empty canvas

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,19 @@
 /**
  * Created by minkwon on 4/04/2016.
  */
+
+function alertOne(x) {
+    return x + 1;
+}
+
+var fps = { startTime : 0, frameNumber : 0,
+    getFPS : function() {
+        this.frameNumber++;
+        var d = new Date().getTime(), currentTime = ( d - this.startTime ) / 1000, result = Math.floor( ( this.frameNumber / currentTime ) );
+        if( currentTime > 1 ) {
+            this.startTime = new Date().getTime();
+            this.frameNumber = 0;
+        }
+        return result;
+    }
+};
